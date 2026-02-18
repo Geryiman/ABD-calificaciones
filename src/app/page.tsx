1fbbcea422013a251cd7335ad765d2d0b56a4b7b
Geryiman <geryiman0@gmail.com>
2026-02-18 01:10:05 -0600

    Refactorizado de main

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-white text-[#008000] flex items-center justify-center">
-      <h1 className="text-4xl text-[#008000] font-bold">
-        SAIIUT UTTehuacán
-      </h1>
-    </main>
-  );
-}
